9e7000a3a01bb4d8ae98b78cd5acaaba6b69d7c2
Dhruti.Korat <1066@shivohm.in>
2023-01-16 12:18:45 +0530

    Added about component file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,10 +18,10 @@ export default function Navbar(props) {
                             <a className="nav-link" href="/">About</a>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
+                    {/* <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn nav-button" type="submit">Search</button>
-                    </form>
+                    </form> */}
                 </div>
             </div>
         </nav>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
 
     const handleUpClick = () => {
         let newTxt = text.toUpperCase();
         setText(newTxt);
     }
 
+    const handleLoClick = () => {
+        let newTxt = text.toLowerCase();
+        setText(newTxt);
+    }
+
+    const handleClearClick = () => {
+        let newTxt = '';
+        setText(newTxt);
+    }
+
+    const handleExtraSpClick = () => {
+        let newTxt = text.trim().split(/ +/).join(' ');
+        setText(newTxt);
+    }
     const handleOnChange = (event) => {
-        console.log(text.length);
         setText(event.target.value);
     }
+
     return (
         <>
             <div className='container'>
@@ -19,14 +33,15 @@ export default function TextForm(props) {
                 <div className="mb-3 my-4">
                     <textarea className="form-control" onChange={handleOnChange} id="myBox" rows="8" value={text}></textarea>
                 </div>
-                <button className="btn nav-button" onClick={handleUpClick}>
-                    Convert to uppercase
-                </button>
+                <button className="btn nav-button" onClick={handleUpClick}> Convert to uppercase </button>
+                <button className="btn nav-button m-2" onClick={handleLoClick}> Convert to lowercase </button>
+                <button className="btn nav-button m-2" onClick={handleClearClick}> Clear text </button>
+                <button className="btn nav-button m-2" onClick={handleExtraSpClick}> Remove extra spaces</button>
             </div>
             <div className='container'>
                 <h1 className="h2 my-4">Your text summary</h1>
-                <p> {text.split(" ").length} words and {text.length} characters.</p>
-                <p> {0.008 * text.split(" ").length} Minutes read</p>
+                <p> {text.trim().split(" ").length} words and {text.length} characters.</p>
+                <p> {0.008 * text.trim().split(" ").length} Minutes read</p>
                 <h2 className='h2 mt-2'> Preview </h2>
                 <p>{text}</p>
             </div>
